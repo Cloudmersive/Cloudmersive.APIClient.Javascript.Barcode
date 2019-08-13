e3d52c1f3becb9e35e68b637ab6eba998838c592
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-13 15:50:04 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 & npm i
 & browserify main.js > cloudmersive-barcode-browser-jsclient.js
+& bower register cloudmersive-apiclient-javascript-barcode https://github.com/Cloudmersive/Cloudmersive.APIClient.Javascript.Barcode.git
